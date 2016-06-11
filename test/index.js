d23f768e8f45b3c2fe21df5f6909c5f19619842e
Ido schachter <id0Sch@users.noreply.github.com>
2016-06-11 13:34:50 +0300

    updated dev dependancies to latest versions updated test to use newer co syntax updated travis.yml to use newer node version

--- test/index.js
+++ test/index.js
@@ -1,14 +1,15 @@
-var expect = require('expect.js'),
-    co = require('co'),
-    sleep = require('..');
+'use strict';
+
+const expect = require('expect.js');
+const co = require('co');
+const sleep = require('..');
 
 describe('co-sleep', function() {
-  it('should sleep for ms', function(done) {
-    co(function *() {
-      var now = Date.now();
-      yield sleep(1000);
-      expect(Date.now() - now).to.not.be.below(1000);
-    })();
-    done();
-  });
+    it('should sleep for ms', co.wrap(function*() {
+        const SLEEP_TIME = 1000;
+        let before = Date.now();
+        yield sleep(SLEEP_TIME);
+        let after = Date.now() - before;
+        expect(after).to.not.be.below(SLEEP_TIME);
+    }));
 });
